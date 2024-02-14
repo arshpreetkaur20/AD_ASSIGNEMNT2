d27e9fe1ebc9590f4bb0b16f9da5688f697290e4
Arshpreet Kaur <arshpreetk498@gmail.com>
2024-02-14 20:29:17 +0530

    made a jar file

---
+++ src/main/java/assignment2/mypkg.java
@@ -0,0 +1,20 @@
+//package assignment2;
+
+//public class mkpkg {
+
+//}
+package assignment2;
+
+//import pkg1.MyClass;
+
+public class mypkg {
+	public void show() {
+		System.out.println("Hello Devops!");
+	}
+
+	public static void main(String[] args) {
+		mypkg ob = new mypkg();
+		ob.show();
+	}
+
+}
